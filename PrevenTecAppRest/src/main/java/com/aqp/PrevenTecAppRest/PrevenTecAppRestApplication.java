d5b1096b9649fba7e052f1f5c8c35d3c0a851d18
NomadaArlequin <jepachauri@gmail.com>
2023-11-21 20:58:55 -0500

    Inicio app rest full

---
+++ PrevenTecAppRest/src/main/java/com/aqp/PrevenTecAppRest/PrevenTecAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.aqp.PrevenTecAppRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevenTecAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevenTecAppRestApplication.class, args);
+	}
+
+}
---
+++ PrevenTecAppRest/src/test/java/com/aqp/PrevenTecAppRest/PrevenTecAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqp.PrevenTecAppRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevenTecAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
